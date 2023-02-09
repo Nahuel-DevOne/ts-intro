378163181e62a4db67c871023b4be9f66b77277a
Nahuel <nahuel.profesor@gmail.com>
2023-02-09 20:44:05 -0300

    Agregando tipos básicos

---
+++ src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,12 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+let nombre: string = 'Nahuel';
+let hp: number | string = 99;
+let estaVivo: boolean = true;
+
+hp = 'FULL'
+
+console.log(nombre, hp);
+
