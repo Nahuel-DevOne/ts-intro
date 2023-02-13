52bf3131c0400582d3ccd57d68afc777be56b3c5
Nahuel <nahuel.profesor@gmail.com>
2023-02-12 21:04:31 -0300

    Agregando desestructuración de argumentos

---
+++ src/ejercicios/06-desestructuracion-argumentos.ts
@@ -0,0 +1,46 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+interface Producto {
+    desc: string;
+    precio: number;
+}
+
+const telefono: Producto = {
+    desc: 'Nokia A1',
+    precio: 150
+}
+
+const tableta: Producto = {
+    desc: 'Ipad Air',
+    precio: 350
+}
+
+function calculaISV( productos: Producto[] ):[number, number] {
+
+    let total = 0;
+    
+    // productos.forEach( producto => {
+    //     total += producto.precio;
+    // })
+
+    // Lo mismo haciendo destructuring de argumentos
+    productos.forEach( ({ precio }) => {
+        total += precio;
+    })
+
+    return [total, total * 0.15];
+
+}
+
+const articulos = [telefono, tableta];
+
+// const isv = calculaISV(articulos);
+// console.log('ISV:', isv);
+
+const [ total, isv ] = calculaISV(articulos);
+console.log('Total:', total);
+console.log('ISV:', isv);
+
+
